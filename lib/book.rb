708800f9c9b5315659d6e473821ba7c2a2f8c3ec
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-27 21:47:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,14 @@
 class Book 
   attr_accessor :author, :page_count, :genre 
   attr_reader :title 
+  
   def initialize(title)
     @title = title 
   end 
   
-  def title 
-    @title 
-  end 
+  # def title 
+  #   @title 
+  # end 
   
   # def author=(author)
   #   @author = author
