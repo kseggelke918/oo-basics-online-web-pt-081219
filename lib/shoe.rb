6aed5956c0b22f17b3f9851ca832357ebeb025fb
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-27 21:50:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,17 +1,12 @@
 class Shoe
+  attr_accessor :color 
+  attr_reader :brand 
+  
   def initialize(brand)
     @brand = brand 
   end 
   
-  def brand
-    @brand 
-  end 
-  def color=(color)
-    @color = color
-  end 
-  
-  def color 
-    @color
-  end 
+
+
   
 end 
